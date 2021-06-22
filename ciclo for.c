79b8afd110c0d781bba1976fa573be4da76a328d
Zeus Xaloc Developer <85583693+ZeusXaloc-Dev@users.noreply.github.com>
2021-06-22 03:02:53 -0400

    Add files via upload

---
+++ ciclo for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    int loop;
+    int numero = 1;
+    
+    for(loop = 1; loop <=10; loop = loop + 1){
+    
+    printf("Testando!");
+    
+    }
+   
+
+    return 0;
+}
